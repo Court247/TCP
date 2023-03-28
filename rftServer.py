4eac5ef818101a3e383a1129342cedeff54ff839
Court247 <courtkneel#yahoo.com>
2023-03-28 13:34:18 -0500

    complete

--- rftServer.py
+++ rftServer.py
@@ -2,13 +2,17 @@
 
 # Import socket module
 from socket import *
+import os
 import sys
 
 # Create a socket object
-s = socket(AF_INET, SOCK_STREAM)
+server = socket(AF_INET, SOCK_STREAM)
 
 #max size of bytes
 size = 1000
+
+#encode type
+FORMAT = "utf-8"
 
 #set host address
 host = '0.0.0.0'
@@ -17,25 +21,44 @@ host = '0.0.0.0'
 port = int(input("Provide Port number: "))
 
 # Bind to the port
-s.bind(('' , port))
+server.bind((host , port))
 
 # Now wait for client connection.
-s.listen(100)
+server.listen(100)
 
+while(True):
 
-# Establish connection with client.
-clientS, addr = s.accept()
+	print("Listening for connection at ", port)
+	
+	# Establish connection with client.
+	client, addr = server.accept()
+	
+	print("Connection accepted from ", addr)
+	
+	#recieve file name from client
+	f = client.recv(size).decode()
+	getFileName = f.split(" ")
+	fileName = getFileName[1]
+	
+	fileinfo = os.stat(fileName)
+	filesize = str(fileinfo.st_size)
+	
+	print("Asking for file ", fileName)
+	
+	#client.send(filesize.encode(FORMAT))
+	
+	with open(fileName, "r") as file:
+		data = file.read()
+		print("Sending the file...")
+		client.send(data.encode(FORMAT))
+		
+	
+	#close the file
+	file.close()
+	
+	print("Transfer Complete!")
+	
+	#close the server
+	server.close()
 
-print("Provide filename: ")
-fileName = input()
-
-with open(fileName,'rb') as file:
-    s.storbinary(f"STOR {fileName}", file, size)
-
-print("File successfully uploaded")
-file.close()
-
-print("Closing connection")
-s.close()
-
-print("Connection closed")
+	print("Connection closed, See you later!")
